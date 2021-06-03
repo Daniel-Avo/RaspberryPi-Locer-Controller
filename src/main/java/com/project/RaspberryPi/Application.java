03dd3f197387a8ad1ec7efd7454a713ef7c01e05
Daniel-Avo <daniel.avoyan21@gmial.com>
2021-06-03 23:43:41 +0200

    Test

--- src/main/java/com/project/RaspberryPi/Application.java
+++ src/main/java/com/project/RaspberryPi/Application.java
@@ -9,7 +9,6 @@ public class Application {
     public static void main(String[] args) throws InterruptedException {
 
         greeting();
-        light();
     }
 
     public static String greeting() {
