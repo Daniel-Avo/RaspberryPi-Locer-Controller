cb8667dd3ccdd1bcd493c0fb562b858c82fe1927
Daniel-Avo <daniel.avoyan21@gmial.com>
2021-06-03 15:58:06 +0200

    Testing light

--- src/main/java/com/project/RaspberryPi/Application.java
+++ src/main/java/com/project/RaspberryPi/Application.java
@@ -1,9 +1,32 @@
 package com.project.RaspberryPi;
 
+import com.pi4j.io.gpio.*;
 
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println("hello world!");
+    private static GpioPinDigitalOutput pin = null;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        greeting();
+        light();
+    }
+
+    public static String greeting() {
+        return "Hello world!";
+    }
+
+    public static void light() throws InterruptedException {
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "LED", PinState.LOW);
+        }
+
+        for (;;){
+            Thread.sleep(1000);
+            pin.toggle();
+        }
+
     }
 }
+
