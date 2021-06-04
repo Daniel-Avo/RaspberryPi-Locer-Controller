420275a7fc10bb256df88eeed38d3f70a7143c0d
Daniel-Avo <daniel.avoyan21@gmial.com>
2021-06-04 13:31:38 +0200

    Adding Spring

--- src/main/java/com/project/RaspberryPi/Application.java
+++ src/main/java/com/project/RaspberryPi/Application.java
@@ -1,8 +1,9 @@
 package com.project.RaspberryPi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class Application {
 
 
