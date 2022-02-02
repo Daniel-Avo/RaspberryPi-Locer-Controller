32fe794b16991d4c8f81f0cecaf17a56903c32ca
Daniel <Daniel@DESKTOP-7IMDU22>
2022-02-03 00:23:56 +0100

    Older version

--- src/main/java/com/project/RaspberryPi/controler/GPIOController.java
+++ src/main/java/com/project/RaspberryPi/controler/GPIOController.java
@@ -4,8 +4,6 @@ import com.pi4j.io.gpio.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class GPIOController {
 
